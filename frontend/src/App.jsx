cae10f1122a8ca65232a032ca63a4cd4e1030386
astaylor11413 <astaylor140@gmail.com>
2026-09-21 20:30:00 -0400

    fix: removed before id to allow map box to render naturally

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -881,15 +881,15 @@ export default function App() {
         }
       }}
     />
-    <Layer {...parishRiskLineLayer} beforeId="substation-layer"/>
+    <Layer {...parishRiskLineLayer}/>
   </Source>
 )}
 
         {/* Dynamic Preventative Green Infrastructure Vector Render */}
         {geoJson.greenInfrastructureGeoJson && (
           <Source id="green-infrastructure-data" type="geojson" data={geoJson.greenInfrastructureGeoJson}>
-            <Layer {...greenInfrastructureFillLayer} beforeId="substation-layer" />
-            <Layer {...greenInfrastructureLineLayer} beforeId="substation-layer"/>
+            <Layer {...greenInfrastructureFillLayer} />
+            <Layer {...greenInfrastructureLineLayer} />
           </Source>
         )}
           {/* 4. GNN SUBSTATION NODES - RENDERED DIRECTLY TO DARK MAP BASE */}
